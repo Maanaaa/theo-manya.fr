163bf6e7b5b773480fde41d1139e9b3b5b183b8f
Théo <manya.th@icloud.com>
2025-04-08 10:08:34 +0200

    index.php terminé et stylisé

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Théo Manya - Portfolio</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h2>MANYA Théo</h2>
+            <ul>
+                <li>Projets</li>
+                <li>Compétences</li>
+                <li>Expériences</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    <div>
+        <img src="img/header.png" alt="Portfolio de Manya Théo, SAE203. Image principale du header.">
+        <h1>Mon Portfolio</h1>
+    </div>
+    <section id="projets">
+        <h2>Mes projets</h2>
+        <div class="card">
+            <p>$compétence</p>
+            <div class="bottom">
+                <p>titre</p>
+                <p>date</p>
+            </div>
+        </div>
+    </section>
+    <section id="skills">
+
+    </section>
+    <section id="experiences">
+        <div class="float left">
+
+        </div>
+        <div class="float right">
+            <img src="img/xp_illustr.png" alt="Portfolio de Manya Théo, SAE203. Image d'illustration de la section expériences.">
+        </div>
+    </section>
+
+
+    </main>
+
+</body>
+</html>
