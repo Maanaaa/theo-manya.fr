a422222a9ee4441e5ea39622a0886ae19069486f
Théo <manya.th@icloud.com>
2025-04-09 11:55:41 +0200

    Ajout de deleteDate pour remplacer tous les fichiers deleteX...

--- scripts/deleteProject.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("../config/configuration.php");
-include("../scripts/connection.php");
-
-$requete = 'DELETE FROM Projets WHERE id_projet ='.$_GET['id_projet'];
-$resultats = $connection->exec($requete);
-header('Location: ../dashboard.php');
