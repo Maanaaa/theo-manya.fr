0e7e56326c6a66c75b1af155bf95d3cc71d3bb2f
Théo <manya.th@icloud.com>
2025-09-22 23:33:27 +0200

    Ajout d'un favicon

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ $resultats->closeCursor();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Théo Manya - Portfolio développeur web & digital designer</title>
+  <link rel="icon" type="image/svg+xml" href="img/favicon.svg">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Portfolio de Théo Manya, développeur web et digital designer. Découvrez mes projets, compétences et expériences.">
   <meta name="author" content="Théo Manya">
